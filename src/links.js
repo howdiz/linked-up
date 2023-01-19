9da9e46f0255a22073f2484afbb246dc1ef87126
Howie Ross <howard.ross@gmail.com>
2023-01-19 11:08:47 -0500

    update with my info

--- src/links.js
+++ src/links.js
@@ -1,29 +1,33 @@
 export const mainContent = {
   imgSrc: `/assets/profile.png`,
-  name: `Little Sticks`,
-  tagline: `Web Development Learning Platform`
+  name: `Howie Ross`,
+  tagline: `Product @edgio, twin dad, coffee geek`,
+  bio1: `I’ve been building products on the internet for 20 years and leading teams for 10 years.`,
+  bio2: `I have worked for startups, agencies, and financial services companies, building websites, apps, e-commerce systems, and, cloud / edge platforms.`,
+  bio3: `I've also lectured for the UX/IxD Masters program at Thomas Jefferson University and spoke at conferences, meetups, and boot camps.`,
+  bio4: `I used to have a lot of hobbies like brewing beer, organize meetups including CTO School Philly, and be on charitable organization boards like the Philly Awesome Foundation. Now, I'm happily the father of twin boys, and I'm very into coffee.`
 };
 
 export const socialLinks = [
   {
     title: 'Instagram',
     icon: 'mdi:instagram',
-    url: 'https://www.instagram.com/jaydanurwin/',
+    url: 'https://www.instagram.com/howdiz/',
   },
   {
     title: 'Twitter',
     icon: 'mdi:twitter  ',
-    url: 'https://twitter.com/littlesticksdev',
+    url: 'https://twitter.com/howdiz',
   },
   {
-    title: 'YouTube',
-    icon: 'mdi:youtube',
-    url: 'https://youtube.com/c/LittleSticks',
+    title: 'Linkedin',
+    icon: 'mdi:linkedin',
+    url: 'https://www.linkedin.com/in/howardross',
   },
   {
     title: 'GitHub',
     icon: 'mdi:github',
-    url: 'https://github.com/littlesticks',
+    url: 'https://github.com/howdiz',
   }
 ]
 
